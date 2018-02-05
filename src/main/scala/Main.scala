cdcbc62a383bc3dc325ce0d7ca5a9f9ba8005360
Rachit Nigam <rachit.nigam12@gmail.com>
2018-02-04 21:20:31 -0500

    first

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,27 @@
+import hw.json._
+import hw.combinator.CombinatorLike
+
+object Combinators extends CombinatorLike {
+
+  def key(key: String) = new JsonProc[Json,Json](json => json match {
+    case JsonDict(map) => {
+      if(map.contains(key)) List(map.get(key))
+      else Nil
+    }
+    case v => Nil
+  })
+
+
+  def number: JsonProc[Json,Double] = ???
+  def index(n: Int): JsonProc[Json,Json] = ???
+  def iter: JsonProc[Json,Json] = ???
+  def addAll(json: Json): Double = ???
+
+  def recur: JsonProc[Json, Json] = ???
+  def split[S]: JsonProc[S, (S, S)] = ???
+  def swap[A, B]: JsonProc[(A, B), (B, A)] = ???
+  def first[A, B, C](proc: JsonProc[A, B]): JsonProc[(A, C), (B, C)] = ???
+  def second[A, B, C](proc: JsonProc[A, B]): JsonProc[(C, A), (C, B)] = ???
+  def combine[A]: JsonProc[(A, A), A] = ???
+
+}
