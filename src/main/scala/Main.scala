9c140cae17391a2144335c60b821fed05ec610b4
Rachit Nigam <rachit.nigam12@gmail.com>
2018-02-05 15:01:04 -0500

    lib update;

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,17 +11,19 @@ object Combinators extends CombinatorLike {
     case v => Nil
   })
 
-
   def number: JsonProc[Json,Double] = ???
   def index(n: Int): JsonProc[Json,Json] = ???
   def iter: JsonProc[Json,Json] = ???
-  def addAll(json: Json): Double = ???
-
   def recur: JsonProc[Json, Json] = ???
   def split[S]: JsonProc[S, (S, S)] = ???
   def swap[A, B]: JsonProc[(A, B), (B, A)] = ???
   def first[A, B, C](proc: JsonProc[A, B]): JsonProc[(A, C), (B, C)] = ???
   def second[A, B, C](proc: JsonProc[A, B]): JsonProc[(C, A), (C, B)] = ???
   def combine[A]: JsonProc[(A, A), A] = ???
+
+  def addAll(json: Json): Double = ???
+  def allNumbers(json: Json): List[Double] = ???
+  def calculateAge(json: Json): Option[Double] = ???
+  def extractNameAndAge(json: Json): Option[(Json, Json)] = ???
 
 }
